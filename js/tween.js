25395f6dd691d94e04c79e1f821c34624f9c6351
sexfat <bryant6831@gmail.com>
2018-02-04 12:01:13 +0800

    add parallax

--- js/tween.js
+++ js/tween.js
@@ -4,12 +4,15 @@ $(function () {
         x: -300,
         // y: -30,
         opacity: 0,
-        scale:1
+        scale: 1
     }, {
         x: 0,
         // y: 0,
-        // ease: Elastic.easeOut.config(1, 0.3),
+        ease: Elastic.easeOut.config(1, 0.3),
+        repeat: 2,
+        repeatDelay: 1,
         opacity: 1,
+        yoyo: true,
         scale: 1.5
     });
 
@@ -26,8 +29,61 @@ $(function () {
     });
 
 
+    var objects = ['.box1', '.box2', '.box3', '.box4'];
+
+    TweenMax.staggerFromTo(objects, 0.3, {
+        x: 0
+    }, {
+        x: 100,
+        repeat: -1,
+        repeatDelay: -1,
+        yoyo: true,
+        // ease: Bounce.easeOut
+    }, 0.5)
+
+    // Timelinemax
+
+    var tl = new TimelineMax({
+        repeat: 1,
+        repeatDelay: 1
+    });
 
 
 
+
+    tl.add(TweenMax.from(".box01", 1, {
+        x: 0,
+        y: 400,
+        opacity: 1,
+        ease: Circ.easeInOut
+    }));
+
+    tl.add(TweenMax.from(".box02", 1, {
+        x: 300,
+        y: 0,
+        delay: 2,
+        opacity: 1,
+        ease: Circ.easeInOut
+    }));
+
+    tl.add(TweenMax.from(".box03", 1, {
+        x: 100,
+        y: 0,
+        delay: 1,
+        opacity: 1,
+        ease: Circ.easeInOut
+    }));
+
+    tl.add(TweenMax.from(".box04", 1, {
+        x: 100,
+        y: 500,
+        delay: 1,
+        opacity: 1,
+        ease: Circ.easeInOut
+    }));
+
+
+    var scene = document.getElementById('scenes');
+    var parallax = new Parallax(scene);
 
 })
